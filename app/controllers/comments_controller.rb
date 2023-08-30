ef7139a2ce493facfacac924f760a4d1429948c3
orange-soba <t.sato.terusa6k@gmail.com>
2023-08-30 17:18:30 +0900

    コメント機能修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,6 +4,8 @@ class CommentsController < ApplicationController
     if @comment.save
       redirect_to prototype_path(@comment.prototype_id)
     else
+      @prototype = Prototype.find(params[:prototype_id])
+      @comments = @prototype.comments
       render "prototypes/show"
     end
   end
